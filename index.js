42df720c9d52626ac3f02c71ecbc1ef1d6818253
Roswell <chrisrogers1988@icloud.com>
2021-09-08 23:12:14 +0100

    Updated to new FXv2 Style fxmanifest.lua files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const defaultOptions = {
   itemsFromCompilation: compilation => Object.keys(compilation.assets),
-  output: '../__resource.lua',
+  output: '../fxmanifest.lua',
 };
 
 function ResourceManifestPlugin(options) {
@@ -29,7 +29,15 @@ ResourceManifestPlugin.prototype.apply = function(compiler) {
 
 function format(assets, path) {
   return `
-ui_page "${path}/index.html"
+  name 'resource name'
+  fx_version 'cerulean'
+  games { 'gta5', 'rdr3 }
+  
+  shared_scripts { 'config.lua' }
+  client_scripts { 'client.lua' }
+  server_scripts { 'server.lua' }
+  
+  ui_page "${path}/index.html"
 
 files{${assets.map(asset => `"${path}/${asset}"`).join(',')}}
   `;
